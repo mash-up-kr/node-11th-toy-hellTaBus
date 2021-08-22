1bc6d5ac4dbdb709da5eb04be59d9cd3c93822f1
nari1021 <y.shhh000@gmail.com>
2021-08-22 14:51:54 +0900

    create :: google code convention applies to all cases.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
+    "google"
   ],
   root: true,
   env: {
@@ -16,9 +17,8 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    "new-cap": 0,
+    "require-jsdoc": 0,
+    "strict": 0,
   },
 };
