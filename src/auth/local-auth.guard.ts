16a8580ec8f3ce7c584df6e2f2c779fffdce1c5a
ujusy <lsyeon1997@ajou.ac.kr>
2021-09-03 03:07:44 +0900

    create :: add local, jwt passport strategy

---
+++ src/auth/local-auth.guard.ts
@@ -0,0 +1,12 @@
+import {ExecutionContext, Injectable} from '@nestjs/common';
+import {AuthGuard} from '@nestjs/passport';
+
+@Injectable()
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const ca = await super.canActivate(context);
+    if (ca) {
+      return true;
+    }
+  }
+}
