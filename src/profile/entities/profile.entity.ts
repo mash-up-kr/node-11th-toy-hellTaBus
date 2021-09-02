70945059e4713f86124aa7794854efd0d2dd05db
ujusy <lsyeon1997@ajou.ac.kr>
2021-09-02 23:58:38 +0900

    create :: profile generate crud

---
+++ src/profile/entities/profile.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../../user/entities/user.entity';
+
+@Entity('profile')
+export class Profile {
+    @PrimaryGeneratedColumn('increment')
+    id: number;
+
+    @Column({length: 30})
+    nickname: string;
+
+    @Column('date')
+    birthday: Date;
+
+    @Column('timestamp', {name: 'created_at', default: () => 'now()'})
+    createdAt: Date;
+
+    @Column('timestamp', {name: 'updated_at', default: () => 'now()'})
+    updatedAt: Date;
+
+    @OneToOne(() => User)
+    @JoinColumn()
+    user: User;
+}
