9bd060b5fe9047911c2998092e97231629e1f87c
ujusy <lsyeon1997@ajou.ac.kr>
2021-09-02 23:57:32 +0900

    update :: createUser API

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -8,20 +8,8 @@ export class User {
   @Column({length: 20})
   password: string;
 
-  @Column({length: 30})
-  nickname: string;
-
   @Column({length: 255, unique: true})
   email: string;
-
-  @Column('date')
-  birthday: Date;
-
-  @Column('timestamp', {name: 'created_at', default: () => 'now()'})
-  createdAt: Date;
-
-  @Column('timestamp', {name: 'updated_at', default: () => 'now()'})
-  updatedAt: Date;
 
   @Column('timestamp', {name: 'deleted_at', nullable: true})
   deletedAt: Date | null;
