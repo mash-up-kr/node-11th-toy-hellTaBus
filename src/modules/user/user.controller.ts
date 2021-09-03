eaf07ad6122a5527fe704f8679a7c7605fccad0b
minidonut <nycom13@gmail.com>
2021-09-03 22:15:04 +0900

    feat(user-doc): improve type-strength

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -12,31 +12,31 @@ export class UserController {
   constructor(private readonly usersService: UserService) {}
 
   @Post()
-  @ApiDocs.createUser()
+  @ApiDocs.createUser('사용자 생성 API')
   createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.createUser(createUserDto);
   }
 
   @Get()
-  @ApiDocs.getAllUser()
+  @ApiDocs.getAllUser('모든 사용자 조회')
   getAllUser(): Promise<User[]> {
     return this.usersService.getAllUser();
   }
 
   @Get(':id')
-  @ApiDocs.getUser()
+  @ApiDocs.getUser('특정 사용자 조회')
   getUser(@Param('id') id: number): Promise<User> {
     return this.usersService.getUser(id);
   }
 
   @Patch('/:id')
-  @ApiDocs.updateUser()
+  @ApiDocs.updateUser('사용자 상세정보 수정')
   updateUser(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
-  @ApiDocs.deleteUser()
+  @ApiDocs.deleteUser('사용자 삭제')
   deleteUser(@Param('id') id: number): Promise<void> {
     return this.usersService.deleteUser(id);
   }
